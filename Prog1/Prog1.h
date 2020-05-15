b0263df3ea9e772d90f153b3c1297713a22ffc84
Edgar Villagran <62895634+EdVillagran@users.noreply.github.com>
2020-05-15 11:42:39 -0500

    Update Prog1.h

--- Prog1/Prog1.h
+++ Prog1/Prog1.h
@@ -3,7 +3,7 @@ Edgar Villagran
 Z1861047
 9/06/2018
 Section 3 
-Csci340
+CSCI340
 */
 
 #include<algorithm>
@@ -14,14 +14,16 @@ using namespace std;
 #ifndef PROG1_H
 #define PROG1_H
 
-const int VEC_SIZE = 250;
-const int NO_ITEMS = 12;
-const int ITEM_W = 5;
-const int LOW = 1;
-const int HIGH = 10000;
+//Defining all the constant variables to use
+const int VEC_SIZE = 250; //Vector Size
+const int NO_ITEMS = 12;  
+const int ITEM_W = 5;     //Width of table to print
+const int LOW = 1;        //Lowest generated number
+const int HIGH = 10000;   //Highest generated number
 
-void genRndNums(vector<int>&v);
-void printVec(const vector<int>& v);
+//Functions we will use
+void genRndNums(vector<int>&v);   //Generate random numbers to fill vector
+void printVec(const vector<int>& v); //Print out vector as table
 
 
 #endif // !PROGRAM1_H
